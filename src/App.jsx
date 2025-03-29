8737b95e7e67030814887bdb49a796a75d10daa1
Elias Grönholm <eliasgronholm03@gmail.com>
2025-03-29 12:08:26 +0200

    wireframe

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Steps from './components/Steps'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Steps />
+    </div>
+  )
+}
+
+
+
+export default App
