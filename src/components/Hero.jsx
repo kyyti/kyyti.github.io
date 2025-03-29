e0abb64c2496a15f620b7e314fd847298bfdb8da
Elias Grönholm <eliasgronholm03@gmail.com>
2025-03-29 13:00:11 +0200

    added color palette

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,16 @@
 const Hero = () => {
     return (
-<section className="dark:bg-gray-100 dark:text-gray-800">
+<section className="bg-[var(--primary-color)] text-[var(--text-color)]">
 	<div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
 		<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-			<h1 className="text-5xl font-bold leading-none sm:text-6xl">Ac mattis
-				<span className="dark:text-violet-600">senectus</span>erat pharetra
+			<h1 className="text-5xl font-bold leading-none sm:text-6xl">Ac mattis   
+				<span className="text-[var(--text-color)]">senectus</span>erat pharetra
 			</h1>
 			<p className="mt-6 mb-8 text-lg sm:mb-12">Dictum aliquam porta in condimentum ac integer
 				<br  className="hidden md:inline lg:hidden" />turpis pulvinar, est scelerisque ligula sem
 			</p>
 			<div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Suspendisse</a>
+				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded bg-[var(--primary-color)] dark:text-gray-50">Suspendisse</a>
 				<a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-800">Malesuada</a>
 			</div>
 		</div>
--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -1,24 +1,24 @@
 const Steps = () => {
     return(
-        <section className="p-6 dark:bg-gray-100 dark:text-gray-800">
+        <section className="p-6 bg-[var(--primary-color)] text-[var(--text-color)]">
 	<div className="container mx-auto">
-		<span className="block mb-2 text-xs font-medium tracking-widest text-center uppercase dark:text-violet-600">How it works</span>
-		<h2 className="text-5xl font-bold text-center dark:text-gray-900">Building with Mamba is simple</h2>
+		<span className="block mb-2 text-xs font-medium tracking-widest text-center uppercase text-[var(--accent-color)]">How it works</span>
+		<h2 className="text-5xl font-bold text-center text-[var(--text-color)]">Building with Mamba is simple</h2>
 		<div className="grid gap-6 my-16 lg:grid-cols-3">
-			<div className="flex flex-col p-8 space-y-4 rounded-md dark:bg-gray-50">
-				<div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full dark:bg-violet-600 dark:text-gray-50">1</div>
+			<div className="flex flex-col p-8 space-y-4 rounded-md bg-[var(--secondary-color)] text-[var(--text-color)]">
+				<div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-[var(--accent-color)] text-[var(--button-text-color)]">1</div>
 				<p className="text-2xl font-semibold">
 					<b>Nulla.</b>Nostrum, corrupti blanditiis. Illum, architecto?
 				</p>
 			</div>
-			<div className="flex flex-col p-8 space-y-4 rounded-md dark:bg-gray-50">
-				<div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full dark:bg-violet-600 dark:text-gray-50">2</div>
+			<div className="flex flex-col p-8 space-y-4 rounded-md bg-[var(--secondary-color)] text-[var(--text-color)]">
+				<div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-[var(--accent-color)] text-[var(--button-text-color)]">2</div>
 				<p className="text-2xl font-semibold">
 					<b>Accusantium.</b>Vitae saepe atque neque sunt eius dolor veniam alias debitis?
 				</p>
 			</div>
-			<div className="flex flex-col p-8 space-y-4 rounded-md dark:bg-gray-50">
-				<div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full dark:bg-violet-600 dark:text-gray-50">3</div>
+			<div className="flex flex-col p-8 space-y-4 rounded-md bg-[var(--secondary-color)] text-[var(--text-color)]">
+				<div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-[var(--accent-color)] text-[var(--button-text-color)]">3</div>
 				<p className="text-2xl font-semibold">
 					<b>Maxime.</b>Expedita temporibus culpa reprehenderit doloribus consectetur odio!
 				</p>
